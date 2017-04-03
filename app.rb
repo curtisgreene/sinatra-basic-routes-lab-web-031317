855f9f9bc8790276028145b2fd7cc4712ad35a74
curtisgreene <curtis.j.w.greene@gmail.com>
2017-04-03 17:09:34 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Curtis"
+  end
+
+  get '/hometown' do
+    "My hometown is Dryden, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Wait for the Moment by Vulfpeck"
+  end
+  
 end
